e5dbbb7a3e14f4fef657fe4bdc07dcb8c36c28bf
Joana <joanajteixeira@outlook.com>
2019-02-16 13:44:25 +0000

    //Debugging: Use the JavaScript Console to Check the Value of a Variable
    //Use the console.log() method to print the value of the variable a where noted in the code.

---
+++ useJavaScriptConsoleToCheckValueVariable.js
@@ -0,0 +1,16 @@
+//Debugging: Use the JavaScript Console to Check the Value of a Variable
+
+//Use the console.log() method to print the value of the variable a where noted in the code.
+
+
+
+let a = 5;
+let b = 1;
+a++;
+// Add your code below this line
+
+
+let sumAB = a + b;
+console.log(sumAB);
+
+console.log(a);
