a1167645f3956ca2051fe75abec6f3d47eca5514
Joana <joanajteixeira@outlook.com>
2019-02-16 13:44:03 +0000

    //Debugging: Use typeof to Check the Type of a Variable
    //Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
    /*JavaScript recognizes six primitive (immutable) data types: Boolean, Null, Undefined, Number, String, and Symbol (new with ES6) and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.*/

---
+++ use-typeofToCheckTheTypeOfVariable.js
@@ -0,0 +1,13 @@
+//Debugging: Use typeof to Check the Type of a Variable
+
+//Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
+/*JavaScript recognizes six primitive (immutable) data types: Boolean, Null, Undefined, Number, String, and Symbol (new with ES6) and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.*/
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Add your code below this line
+
+
+console.log(typeof seven); //logs number
+console.log(typeof three);//logs string
